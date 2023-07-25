db931b1d628a4e606919513e97207dfe98295c90
Evelyn Olivier <evelynolivier@Evelyns-MacBook-Pro.local>
2023-07-25 13:58:30 -0400

    Add leader commmand for clipboard paste

--- lua/turtlekwitty/init.lua
+++ lua/turtlekwitty/init.lua
@@ -45,6 +45,8 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+vim.keymap.set("n", "<leader>P", [["+P]])
 
 vim.keymap.set("n", "<leader>j", ":cn<CR>")
 vim.keymap.set("n", "<leader>k", ":cl<CR>")
