685fc5617f15652e0cc61bdd9660bc1baf7df2f1
TurtleKwitty <jerimo849@gmail.com>
2023-08-09 13:56:26 -0400

    Use live grep from telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,5 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	local toGrep = vim.fn.input("Grep > ")
-	print(toGrep)
-	builtin.grep_string({search = toGrep});
+	builtin.live_grep()
 end)
--- lua/turtlekwitty/remap.lua
+++ lua/turtlekwitty/remap.lua
@@ -1,4 +1,26 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>m", ":make<CR>")
 vim.keymap.set("n", "<leader>n", ":cn<CR>")
+vim.keymap.set("i", "<C-h>", "<C-o>h", { noremap = false }) --This one already is default (???) but just to be safe
+vim.keymap.set("i", "<C-j>", "<C-o>j", { noremap = false })
+vim.keymap.set("i", "<C-k>", "<C-o>k")
+vim.keymap.set("i", "<C-l>", "<C-o>l")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+vim.keymap.set("n", "<leader>P", [["+P]])
+
+vim.keymap.set("n", "<leader>j", ":cn<CR>")
+vim.keymap.set("n", "<leader>k", ":cl<CR>")
